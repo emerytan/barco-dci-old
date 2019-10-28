ae3a83f865a8b30c11b219351f14796ef73eebaa
Emery Anderson <emery.t.anderson@gmail.com>
2019-10-28 19:10:22 -0400

    front end almost done

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 var connections = []
+var deviceState = {}
+
 
 
 app.use(express.static(__dirname + '/'))
@@ -13,43 +15,61 @@ app.get('/', function (req, res) {
 
 server.listen(3000, () => {
 	console.log('Barco control webApp listening on port 3000')
+	deviceState.lastMac = 18
 })
 
 
 io.on('connection', (socket) => {
+	var macros = require('./test').macros
 	connections.push(socket)
-
+	
 	console.log(`server: number of client connections = ${connections.length}`)
 
 	socket.on('page loaded', () => {
 		console.log('page loaded')
+		io.sockets.emit('macros', macros)
+		io.sockets.emit('power', 1)
+		io.sockets.emit('dowser', 1)
+		io.sockets.emit('last macro', deviceState.lastMac)
 	})
 
 	socket.on('lens command', (data) => {
 		console.log(data)
 
-
 		switch (data.action) {
+			case 'power':
+				console.log(`send power value: ${data.command}`)
+				if (data.command === '0') { // power is on
+					console.log('lamp power is on - send off command');
+					io.sockets.emit('power', 1)
+				} else if (data.command === '1') {
+					console.log('lamp power is off - send on command');
+					io.sockets.emit('power', 0)
+				}
+				break;
+			case 'dowser':
+				console.log(`send dowser value: ${data.command}`)
+				if (data.command === '0') { // power is on
+					console.log('lamp power is on - send off command');
+					io.sockets.emit('dowser', 1)
+				} else if (data.command === '1') {
+					console.log('lamp power is off - send on command');
+					io.sockets.emit('dowser', 0)
+				}
+				break;
 			case 'focus':
 				console.log(`send focus value: ${data.command}`)
-				
-
-				
 				break;
 			case 'zoom':
 				console.log(`send zoom value: ${data.command}`)
-
 				break;
 			case 'shift':
 				console.log(`send shift value: ${data.command}`)
-
 				break;
 			default:
 				console.log('projector command not recognized');
-
 				break;
 		}
-
 	})
 
 	socket.on('disconnect', function (socket) {
--- src/app.js
+++ src/app.js
@@ -1,22 +1,42 @@
 import io from 'socket.io-client'
 var socket = io.connect()
 
-
-
 var appMessages = document.getElementById('appMessages')
 var ioState = document.getElementById('ioState')
 var headerText = document.getElementById('headerText')
-
+var lensButtons = document.getElementsByTagName('input')
+var macroList = document.getElementById('macroList')
+var lampPower = document.getElementById('lampPower')
+var dowser = document.getElementById('dowser')
 
 document.addEventListener('DOMContentLoaded', function () {
 
 	appMessages.innerText = 'page fully loaded'
 	appMessages.style.color = 'green'
-	headerText.innerText = "Barco Web Control - Minimal"
+	headerText.innerText = "CO3 Barco Web Control"
 	headerText.style.color = 'green'
+	
+	document.getElementById('test1').addEventListener('click', () => {
+		appMessages.innerText = 'test1'
+		activateButtons(lensButtons, true)
+	})
+
+	document.getElementById('test2').addEventListener('click', () => {
+		appMessages.innerText = 'test2'		
+		activateButtons(lensButtons, false)
+	})
+
+	document.getElementById('test3').addEventListener('click', () => {
+		appMessages.innerText = 'test3'		
+	})
+
+	document.getElementById('test4').addEventListener('click', () => {
+		appMessages.innerText = 'test4'		
+	})
 
 	document.addEventListener('click', function (event) {
-		if (event.target.matches('.btn')) {
+		console.log(event.target)
+		if (event.target.matches('.lens')) {
 			let CMD = event.target.dataset
 			appMessages.innerText = `action: ${CMD.action}  <--->   command: ${CMD.command}`
 			socket.emit('lens command', {
@@ -25,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function () {
 			})
 		}
 	})
-
+	
 	// sockets
 	socket.on('connect', () => {
 		ioState.innerText = 'Web server online'
@@ -36,18 +56,61 @@ document.addEventListener('DOMContentLoaded', function () {
 		})
 	})
 
-	
+	socket.on('power', (msg) => {
+		ioState.innerText = msg
+		if (msg === 0) {
+			appMessages.innerText = 'lamp is on'
+			lampPower.classList.remove('btn-dark')
+			lampPower.classList.add('btn-success')
+			lampPower.dataset.command = 0
+			lampPower.value = 'On'
+		} else if (msg === 1) {
+			appMessages.innerText = 'lamp is off'
+			lampPower.classList.remove('btn-success')
+			lampPower.classList.add('btn-dark')
+			lampPower.dataset.command = 1
+			lampPower.value = 'Off'
+		} else {
+			appMessages.innerText = 'lamp power command fail'
+		}
+	})
+
+	socket.on('dowser', (msg) => {
+		ioState.innerText = msg
+		if (msg === 0) {
+			appMessages.innerText = 'dowser is open'
+			dowser.classList.remove('btn-dark')
+			dowser.classList.add('btn-success')
+			dowser.dataset.command = 0
+			dowser.value = 'Open'
+		} else if (msg === 1) {
+			appMessages.innerText = 'dowser is closed'
+			dowser.classList.remove('btn-success')
+			dowser.classList.add('btn-dark')
+			dowser.dataset.command = 1
+			dowser.value = 'Closed'
+		} else {
+			appMessages.innerText = 'dowser command fail'
+		}
+	})
+
+	socket.on('macros', (data) => {
+		addMacros(data)
+	})
+
+	socket.on('last macro', (msg) => {
+		macroList.value = msg
+	})
+
 	socket.on('disconnect', () => {
 		ioState.innerText = "Web server down"
 		ioState.style.color = 'red'
 	})
 	
-
 	socket.on('io init', (msg) => {
 		appMessages.innerText = 'io init socket'
 		appMessages.style.color = 'orange'
 	})
-
 
 	socket.on('server messages', (msg) => {
 		appMessages.innerText = msg
@@ -56,4 +119,25 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 
+function activateButtons(elements, action) {
+	var t
+	var i = 0
+	while (t = elements[i++]) {
+		console.log(t);
+		t.disabled = action
+	}
+}
 
+
+function addMacros(macros) {
+	var opt
+	for (const key in macros) {
+		if (macros.hasOwnProperty(key) && macros[key] != '') {
+			opt = document.createElement('option')
+			opt.textContent = macros[key]
+			opt.value = key
+			console.log(opt)
+			macroList.appendChild(opt)
+		}
+	}
+}
